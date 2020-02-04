6491324b3c8b779adc4330e243edf897301fc439
Chen Xiao <chenxiao770117@gmail.com>
2020-02-05 07:55:53 +0800

    update

--- pure-crypto/src/encrypt.rkt
+++ pure-crypto/src/encrypt.rkt
@@ -6,7 +6,6 @@
 (require "lib/process-key.rkt")
 (require "lib/process-data.rkt")
 (require "cipher/des.rkt")
-(require "share.rkt")
 
 (require file/sha1)
 (require net/base64)
@@ -78,80 +77,81 @@
                      [encrypted_block_binary_data #f]
                      [result_binary_data #f])
 
-          (express express? (lambda () (write-report-des-block-start block_index block_binary_data last_result express_path?)))
+                (detail-line (format "----block index:[~a]----" block_index))
+                (detail-line "block_binary_data:")
+                (detail-line block_binary_data #:line_break_length 8)
 
-          (cond
-           [(eq? operation_mode? 'ecb)
-            (set! operated_binary_data block_binary_data)]
-           [(eq? operation_mode? 'cbc)
-            (set! operated_binary_data
-                  (~r #:base 2 #:min-width 64 #:pad-string "0" (bitwise-xor (string->number last_result 2) (string->number block_binary_data 2))))]
-           [(eq? operation_mode? 'pcbc)
-            (set! operated_binary_data
-                  (let* ([step1 (bitwise-xor (string->number last_result 2) (string->number last_origin_bin 2))]
-                         [step2 (bitwise-xor (string->number block_binary_data 2) step1)])
-                    (~r #:base 2 #:min-width 64 #:pad-string "0" step2)))]
-           [(or
-             (eq? operation_mode? 'cfb)
-             (eq? operation_mode? 'ofb))
-            (set! operated_binary_data last_result)]
-           [else
-            (set! operated_binary_data block_binary_data)])
-
-          (express
-           express?
-           (lambda ()
-             (cond
-              [(eq? operation_mode? 'cbc)
-               (write-report-des-cbc-operation block_index last_result block_binary_data operated_binary_data express_path?)]
-              [(eq? operation_mode? 'pcbc)
-               (write-report-des-pcbc-operation block_index last_result last_origin_bin block_binary_data operated_binary_data express_path?)])))
-          
-          (set! encrypted_block_binary_data
-                (if (eq? type? 'des)
-                    (encryption block_index (list-ref k_lists 0) operated_binary_data express? express_path?)
-                    (let ([e1 #f]
-                          [ed2 #f])
-                      (set! e1 (encryption block_index (list-ref k_lists 0) operated_binary_data express? express_path?))
-                      (set! ed2 (decryption block_index reverse_ip_1_table reverse_ip_table (list-ref k_lists 1) e1 express? express_path?))
-                      (encryption block_index (list-ref k_lists 2) ed2 express? express_path?))))
-
-          (set! result_binary_data
                 (cond
+                 [(eq? operation_mode? 'ecb)
+                  (set! operated_binary_data block_binary_data)]
+                 [(eq? operation_mode? 'cbc)
+                  (set! operated_binary_data
+                        (~r #:base 2 #:min-width 64 #:pad-string "0" (bitwise-xor (string->number last_result 2) (string->number block_binary_data 2))))]
+                 [(eq? operation_mode? 'pcbc)
+                  (set! operated_binary_data
+                        (let* ([step1 (bitwise-xor (string->number last_result 2) (string->number last_origin_bin 2))]
+                               [step2 (bitwise-xor (string->number block_binary_data 2) step1)])
+                          (~r #:base 2 #:min-width 64 #:pad-string "0" step2)))]
                  [(or
                    (eq? operation_mode? 'cfb)
                    (eq? operation_mode? 'ofb))
-                  (let* ([padding_before_xor (~a #:min-width 64 #:right-pad-string "0" block_binary_data)]
-                         [cofb_xor_result
-                          (~r #:min-width 64 #:base 2 #:pad-string "0"
-                              (bitwise-xor (string->number encrypted_block_binary_data 2) (string->number padding_before_xor 2)))])
-                    (if (not (= (string-length block_binary_data) 64))
-                        (substring cofb_xor_result 0 (string-length block_binary_data))
-                        cofb_xor_result))]
+                  (set! operated_binary_data last_result)]
                  [else
-                  encrypted_block_binary_data]))
+                  (set! operated_binary_data block_binary_data)])
 
-          (express
-           express?
-           (lambda ()
-             (cond
-              [(or
-                (eq? operation_mode? 'cfb)
-                (eq? operation_mode? 'ofb))
-               (write-report-des-cofb-operation encrypted_block_binary_data block_binary_data result_binary_data express_path?)])))
+                (detail-line "last_result:")
+                (detail-line last_result)
+                (detail-line "last_origin_bin:")
+                (detail-line last_origin_bin)
+                (detail-line "operated_binary_data:")
+                (detail-line operated_binary_data)
 
-          (loop
-           (cdr blocks)
-           (add1 block_index)
-           (cond
-            [(eq? operation_mode? 'ofb)
-             encrypted_block_binary_data]
-            [else
-             result_binary_data])
-           block_binary_data
-           (cons
-            result_binary_data
-            result_list)))
+                (set! encrypted_block_binary_data
+                      (if (eq? type? 'des)
+                          (encryption block_index (list-ref k_lists 0) operated_binary_data express? express_path?)
+                          (let ([e1 #f]
+                                [ed2 #f])
+                            (set! e1 (encryption block_index (list-ref k_lists 0) operated_binary_data express? express_path?))
+                            (set! ed2 (decryption block_index reverse_ip_1_table reverse_ip_table (list-ref k_lists 1) e1 express? express_path?))
+                            (encryption block_index (list-ref k_lists 2) ed2 express? express_path?))))
+
+                (detail-line "encrypted_block_binary_data:")
+                (detail-line encrypted_block_binary_data)
+
+                (set! result_binary_data
+                      (cond
+                       [(or
+                         (eq? operation_mode? 'cfb)
+                         (eq? operation_mode? 'ofb))
+                        (let* ([padding_before_xor (~a #:min-width 64 #:right-pad-string "0" block_binary_data)]
+                               [cofb_xor_result
+                                (~r #:min-width 64 #:base 2 #:pad-string "0"
+                                    (bitwise-xor (string->number encrypted_block_binary_data 2) (string->number padding_before_xor 2)))])
+                          (if (not (= (string-length block_binary_data) 64))
+                              (substring cofb_xor_result 0 (string-length block_binary_data))
+                              cofb_xor_result))]
+                       [else
+                        encrypted_block_binary_data]))
+
+                (when (or
+                       (eq? operation_mode? 'cfb)
+                       (eq? operation_mode? 'ofb))
+
+                  (detail-line "result_binary_data:")
+                  (detail-line result_binary_data))
+
+                (loop
+                 (cdr blocks)
+                 (add1 block_index)
+                 (cond
+                  [(eq? operation_mode? 'ofb)
+                   encrypted_block_binary_data]
+                  [else
+                   result_binary_data])
+                 block_binary_data
+                 (cons
+                  result_binary_data
+                  result_list)))
         (let* ([encrypted_binary_data_list (reverse result_list)]
                [encrypted_data
                 (foldr string-append ""
@@ -166,5 +166,14 @@
                   (bytes->string/utf-8 (base64-encode (hex-string->bytes encrypted_data)))]
                  [else
                   encrypted_data])])
-          (express express? (lambda () (write-report-des-end encrypted_binary_data_list encrypted_data encrypted_format? final_data express_path?)))
-          final_data))))
+          (detail-line "encrypted_binary_data_list:")
+          (detail-simple-list encrypted_binary_data_list #:cols_count 1)
+
+          (detail-line "encryted_hex_data:")
+          (detail-line encrypted_data #:line_break_length 16)
+
+          (detail-line (format "encryted_format?:[~a]" encryted_format?))
+
+          (detail-line final_data #:line_break_length 16)
+
+          final_data))))))))
