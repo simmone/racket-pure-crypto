7ea82db65853810c497c220be9ed1865ec496226
Chen Xiao <chenxiao770117@gmail.com>
2020-03-07 11:15:12 +0800

    update

--- pure-crypto/test/encrypt/aes/aes-test.rkt
+++ pure-crypto/test/encrypt/aes/aes-test.rkt
@@ -12,10 +12,27 @@
    (test-case
     "test-ecb-128"
 
+    ;; TEST-CASE: AES-ECB-128 HEX:HEX:HEX
     (check-equal?
      (encrypt #:cipher? 'aes "0123456789ABCDEF0123456789ABCDEF" "133457799BBCDFF133457799BBCDFFAB"
               #:operation_mode? 'ecb #:data_format? 'hex #:key_format? 'hex)
      "4AE08C70BEA1D25577F34EF92877F787"
+    )
+
+    ;; NIXT: ECB-AES128 HEX:HEX:HEX
+    (check-equal?
+     (encrypt #:cipher? 'aes #:operation_mode? 'ecb #:data_format? 'hex #:key_format? 'hex
+              (string-append
+               "6bc1bee22e409f96e93d7e117393172a"
+               "ae2d8a571e03ac9c9eb76fac45af8e51"
+               "30c81c46a35ce411e5fbc1191a0a52ef"
+               "f69f2445df4f9b17ad2b417be66c3710")
+              "2b7e151628aed2a6abf7158809cf4f3c")
+     (string-append
+      "3AD77BB40D7A3660A89ECAF32466EF97"
+      "F5D3D58503B9699DE785895A96FDBAAF"
+      "43B1CD7F598ECE23881B00E3ED030688"
+      "7B0C785E27E8AD3F8223207104725DD4")
     )
 
     (check-equal?
@@ -122,17 +139,6 @@
      (encrypt #:cipher? 'aes #:operation_mode? 'pcbc
               "chenxiaoxiaochena" "chensihehesichen")
     "3C0AEADD704C4A2FF227CCB67C2F4F6502C1F73B805A0C031741F9F0D9B6CBFD")
-    )
-
-   (test-case
-    "test-ctr-128"
-    
-    (check-equal?
-     (encrypt #:cipher? 'aes #:operation_mode? 'ctr #:data_format? 'hex #:key_format? 'hex
-              #:detail? '(console "detail.pdf")
-              "6BC1BEE22E409F96E93D7E117393172A"
-              "0123456789abcdef0123456789abcdef")
-     "3B3FD92EB72DAD20333449F8E83CFB4A")
     )
 
    ))
