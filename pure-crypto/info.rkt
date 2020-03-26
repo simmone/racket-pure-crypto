70094968bedfac66bb2c09847ed004b92433a36c
Chen Xiao <chenxiao770117@gmail.com>
2020-03-26 14:06:44 +0800

    update

--- pure-crypto/info.rkt
+++ pure-crypto/info.rkt
@@ -1,9 +1,9 @@
 #lang info
 
 (define scribblings
-  '(("scribble/des.scrbl" (multi-page) (tool 100))))
+  '(("scribble/pure-crypto.scrbl" (multi-page) (tool 100))))
 
-(define compile-omit-paths '("tests" "refs"))
-(define test-omit-paths '("tests" "refs"))
+(define compile-omit-paths '("test" "ref"))
+(define test-omit-paths '("test" "ref"))
 
 
